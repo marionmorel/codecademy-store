ed76acb72f0f6ae4bb44d710337b156e32ce3201
ma-mrl <marion.mrl@live.fr>
2024-04-18 20:44:11 +0200

    create the redux store from slice reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,15 @@
 // Import createStore and combineReducers here.
+import { createStore, combineReducers } from 'redux';
 
 // Import the slice reducers here.
+import { inventoryReducer } from '../features/inventory/inventorySlice';
+import { cartReducer } from '../features/cart/cartSlice';
+import { currencyFilterReducer } from '../features/currencyFilter/currencyFilterSlice';
 
 // Create and export the store here.
+export const store = createStore(combineReducers({
+    inventorySlice: inventoryReducer,
+    cartSlice: cartReducer,
+    currencyFilterSlice: currencyFilterReducer 
+    })
+)
